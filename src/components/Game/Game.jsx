53feaf0656cf9469f4caed414bdd66e74a996f58
”David <brayzgunov28@gmail.com>
2022-05-05 16:34:18 +0600

    sdf

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,49 +1,33 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import Loading from '../../UI/Loading/Loading';
-import Question from '../Question/Question';
-import styles from "./Game.module.css"
-import RowItems from "./RowItems"
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import Loading from "../../UI/Loading/Loading";
+import Question from "../Question/Question";
+import styles from "./Game.module.css";
+import RowItems from "./RowItems";
 function Game() {
-  
-  const { categories, show,categs } = useSelector((state) => state.auth);
-  const { statSumPoints, } = useSelector((state) => state.auth.currentGame);
-  
-  
-  
+  const { categories, show, categs } = useSelector((state) => state.auth);
+  const { statSumPoints } = useSelector((state) => state.auth.currentGame);
+
   if (!categories) {
     return <Loading />;
   }
 
   console.log(categs);
- 
+  const data = categs.length >= 5 ? categs : categories;
   return (
     <>
-      {show  && <Question />}
-      {categs.length >= 5 ?
-      <div className={styles.gridCont}>
-        {categs.map((item) => {
-          return (
-            <div key={item.id} className={styles.grid}>
-              <h4>{item.title} </h4>
-             <RowItems  {...item} />
-            </div>
-          );
-        })}
-      </div>
-      :
-      <div className={styles.gridCont}>
-        {categories.map((item) => {
-          return (
-            <div key={item.id} className={styles.grid}>
-              <h4>{item.title} </h4>
-             <RowItems  {...item} />
-            </div>
-          );
-        })}
-      </div>
-      }
+      {show && <Question />}
 
+      <div className={styles.gridCont}>
+        {data.map((item) => {
+          return (
+            <div key={item.id} className={styles.grid}>
+              <h4>{item.title} </h4>
+              <RowItems {...item} />
+            </div>
+          );
+        })}
+      </div>
 
       <div className="count">
         <h1 style={{ color: "white" }}>Счёт {statSumPoints} </h1>
@@ -52,4 +36,4 @@ function Game() {
   );
 }
 
-export default Game
+export default Game;
